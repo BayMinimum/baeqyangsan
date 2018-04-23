ccd979f768780e3e82b33023b05f85fb1042b04e
Sangbum Kim <bearksb1115@gmail.com>
2018-04-23 13:02:21 +0900

    Change to event-driven behavior

--- baeqyangsan.js
+++ baeqyangsan.js
@@ -1,10 +1,15 @@
-var startInput = $('expect_start')
-var endInput = $('expect_end')
-function editable_and_remove_btn(input){
-  if(input){
-    input.removeAttr('readonly')
-    input.next().remove()
+function bys_main(){
+  var startInput = $('expect_start')
+  var endInput = $('expect_end')
+  function editable_and_remove_btn(input){
+    if(input){
+      input.removeAttr('readonly')
+      input.next().remove()
+    }
   }
+  editable_and_remove_btn(startInput)
+  editable_and_remove_btn(endInput)
 }
-editable_and_remove_btn(startInput)
-editable_and_remove_btn(endInput)
+
+window.onEventListener('load', bys_main)
+window.frames[1].onEventListener('load', bys_main)
